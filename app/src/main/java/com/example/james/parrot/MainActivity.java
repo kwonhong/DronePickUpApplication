eabdb5b35817a129a44baa4f006cbc0f60933b53
kwonhong <hongilkwon316@gmail.com>
2017-01-10 23:45:19 -0500

    Setup OpenCV SDK

--- app/src/main/java/com/example/james/parrot/MainActivity.java
+++ app/src/main/java/com/example/james/parrot/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.james.parrot;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.example.james.parrot.app.ParrotApplication;
+
+import org.opencv.android.OpenCVLoader;
+
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
     @Inject Resources resources; // Reference get injected.
+
+    private static final String TAG = "MainActivity";
+
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+        } else {
+            Log.d(TAG, "OpenCV loaded");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
